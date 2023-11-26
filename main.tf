624fe46d1acab1f2caf18c98da7f6a39a2c6aec6
detree05 <125824800+detree05@users.noreply.github.com>
2023-11-26 23:11:41 +0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,107 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.87.0"
+}
+
+provider "yandex" {
+  service_account_key_file = "./authorized_key.json"
+  folder_id                = local.folder_id
+  zone                     = "ru-central1-a"
+}
+
+locals {
+  folder_id = var.folder_id
+  service-accounts = toset([
+    "bingo-sa",
+  ])
+  bingo-sa-roles = toset([
+    "editor"
+  ])
+}
+  
+resource "yandex_vpc_network" "bingo-network" {}
+
+resource "yandex_vpc_subnet" "bingo-subnet" {
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.bingo-network.id}"
+  v4_cidr_blocks = ["10.5.0.0/24"]
+}
+
+resource "yandex_iam_service_account" "service-accounts" {
+  for_each = local.service-accounts
+  name     = each.key
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "bingo-roles" {
+  for_each  = local.bingo-sa-roles
+  folder_id = local.folder_id
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["bingo-sa"].id}"
+  role      = each.key
+}
+
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2204-lts"
+}
+
+resource "yandex_compute_instance" "bingo-db" {
+  platform_id        = "standard-v2"
+  service_account_id = yandex_iam_service_account.service-accounts["bingo-sa"].id
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 5
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.bingo-subnet.id}"
+    ip_address = "10.5.0.15"
+	nat = true
+  }
+  boot_disk {
+    initialize_params {
+      type = "network-hdd"
+      size = "30"
+      image_id = data.yandex_compute_image.ubuntu.id
+    }
+  }
+  metadata = {
+    serial-port-enable = 1
+    user-data = file("${path.module}/database/cloud-init.yaml")
+  }
+}
+
+resource "yandex_compute_instance" "bingo-service" {
+  platform_id        = "standard-v2"
+  service_account_id = yandex_iam_service_account.service-accounts["bingo-sa"].id
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 5
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.bingo-subnet.id}"
+    ip_address = "10.5.0.10"
+    nat = true
+  }
+  boot_disk {
+    initialize_params {
+      type = "network-hdd"
+      size = "30"
+      image_id = data.yandex_compute_image.ubuntu.id
+    }
+  }
+  metadata = {
+    serial-port-enable = 1
+    user-data = file("${path.module}/service/cloud-init.yaml")
+  }
+  depends_on = [yandex_compute_instance.bingo-db]
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "folder_id" {
+	description = "Your YC Folder ID"
+	type 		= string
+	default		= "b1gk9ll6ggr12cpfrj6b"
+	nullable	= false
+}
